896a1c52910b9bd705540557275318dbdd56c434
Paul Martin <communty.hivemind@gmail.com>
2019-03-21 14:38:31 +0000

    finish BirthdayCalc module
    final tests:
    1 - it returns 364 if birthday was yesterday and no leap year
    2 - it returns 365 if birthday was yesterday and yes leap year

--- lib/birthday_calc.rb
+++ lib/birthday_calc.rb
@@ -1,9 +1,9 @@
 require 'date'
 
 module BirthdayCalc
-  def self.countdown(day, month)
-    today = Date.today
-    birthday = DateTime.strptime("#{day} #{month}", '%d %m')
+  def self.countdown(day, month, today = Date.today)
+    birthday = Date.strptime("#{day} #{month}", '%d %m')
+    birthday = birthday.next_year if birthday < today
     (birthday - today).to_i
   end
 end
--- spec/birthday_calc_spec.rb
+++ spec/birthday_calc_spec.rb
@@ -5,19 +5,39 @@ describe BirthdayCalc do
   describe "#countdown" do
     context 'my birthday is today' do
       it 'returns 0' do
-        today = Date.today
-        day = today.day
-        month = today.month
+        birthday = Date.today
+        day = birthday .day
+        month = birthday .month
         expect(described_class.countdown(day, month)).to eq 0
       end
     end
 
     context 'my birthday is tomorrow' do
       it 'returns 1' do
-        today = Date.today + 1
-        day = today.day
-        month = today.month
+        birthday = Date.today + 1
+        day = birthday .day
+        month = birthday .month
         expect(described_class.countdown(day, month)).to eq 1
+      end
+    end
+
+    context 'my birthday was yesterday' do
+      context "and there is a leap day between now and my next birthday" do
+        it 'returns 365' do
+          today = Date.strptime("2019-03-21")
+          day = 20
+          month = 03
+          expect(described_class.countdown(day, month, today)).to eq 365
+        end
+      end
+
+      context "and there isn't a leap day between now and my next birthday" do
+        it 'returns 364' do
+          today = Date.strptime("2018-03-21")
+          day = 20
+          month = 03
+          expect(described_class.countdown(day, month, today)).to eq 364
+        end
       end
     end
   end
