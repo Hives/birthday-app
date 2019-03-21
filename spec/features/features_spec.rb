683ed901cc18c223d450ae46280cf81ed6d06e8d
Paul Martin <communty.hivemind@gmail.com>
2019-03-21 21:50:06 +0000

    update wording of users stories

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -3,7 +3,7 @@ require 'date'
 feature 'The form page' do
   # As a user
   # So that I can feel warm and fuzzy
-  # It should give me a friendly welcome
+  # It want to see a friendly welcome
   scenario "It should give me a warm welcome" do
     visit "/"
     expect(page).to have_content("Hello there!")
@@ -11,7 +11,7 @@ feature 'The form page' do
 
   # As a user
   # So that I can find out how long it is till my birthday
-  # I want to give it my birthday
+  # I want to enter my birthday
   scenario "It should ask for the month of my birthday" do
     visit "/"
     expect(page.find('select[name="month"]'))
@@ -27,7 +27,7 @@ end
 feature 'The response page' do
   # As a user
   # So that I can feel the vibes
-  # It should wish me happy birthday if it's my birthday
+  # I want to be wished happy birthday if it is my birthday
   context "If today is my birthday" do
     scenario "It should wish me happy birthday" do
       birthday = Date.today
@@ -40,7 +40,7 @@ feature 'The response page' do
 
   # As a user
   # So that I can feel the an-tici--------pation
-  # It should tell me how many days it is to my birthday
+  # I want to know how many days it is to my birthday
   context "If my birthday is in five days" do
     before do
       birthday = Date.today + 5
@@ -61,13 +61,14 @@ feature 'The response page' do
 
   # As a grammar pedant
   # So I can avoid being offended
-  # I don't want to see an 's' on the end of 'days' unless necessary
+  # I don't want to see an 's' on the end of 'days' if there's only one
   context "If my birthday is tomorrow" do
     before do
       birthday = Date.today + 1
       @day = birthday.day
       @month = birthday.strftime('%B')
     end
+
     scenario "It should say '1 day' not '1 days'" do
       complete_form("Sarah", @day, @month)
       expect(page).to have_content("Your birthday will be in 1 day, Sarah.")
