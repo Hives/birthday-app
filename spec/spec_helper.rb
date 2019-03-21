362ca1cd8d2a10d26a4a8b2ec0475f6f4a053915
Paul Martin <communty.hivemind@gmail.com>
2019-03-21 16:48:48 +0000

    add simplecov gem to check test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,12 +3,21 @@ ENV['RACK_ENV'] = 'test'
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require 'simplecov'
+require 'simplecov-console'
 
 # require our Sinatra app
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 # require our helper
 require File.join(File.dirname(__FILE__), '.', 'features/web_helpers.rb')
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
 
 # tell Capybara about our app class
 Capybara.app = Birthday
